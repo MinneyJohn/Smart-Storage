f959358ebaa939f82650c6f9e4bd157986ea41de
minneyjohn <root@sm114.localdomain>
2019-05-14 22:28:57 -0700

    Fix

--- statsHelper.py
+++ statsHelper.py
@@ -253,7 +253,7 @@ class IoStats:
 
         # Set the IO trigger time
         self.triggerTime = datetime.datetime.now()
-        (ret, output) = adminHelper.check_output(iostat_cmd)
+        (ret, output) = casAdmin.check_output(iostat_cmd)
         if ret:
             logMgr.info("***ERROR** Failed to get output of iostat")
             return ret
